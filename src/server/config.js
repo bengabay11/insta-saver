33a9a591894073455658fcd6ca50e3a941145c6e
ben <ben.gabay38@gmail.com>
2019-03-21 22:55:24 +0200

    removed compiled typescript files from server side

--- src/server/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    port: 3000,
-    staticFolder: "C:\\Code\\InstaSaver\\src\\client",
-    nodeModulesFolder: "C:\\Code\\InstaSaver\\node_modules"
-};
-//# sourceMappingURL=config.js.map
